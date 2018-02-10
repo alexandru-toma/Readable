af824c13c6cbdb510b6450c45b0b3519f5263e4c
Alexandru Nicu Toma <alexandru.toma.nicu@gmail.com>
2018-02-10 15:48:22 +0200

    Initial structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
@@ -9,4 +10,16 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps () {
+
+  return {}
+}
+
+function mapDispatchToProps (dispatch) {
+  return {}
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
